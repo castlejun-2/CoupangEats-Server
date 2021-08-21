927be8e75138dd93580da5339780031d66296a0b
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-08-21 18:59:18 +0900

    create 2021-08-21.md

--- src/app/Order/orderRoute.js
+++ src/app/Order/orderRoute.js
@@ -5,4 +5,6 @@ module.exports = function(app){
     // 32. 카트에 담기 API
     app.post('/app/orders/:userId/in-cart', jwtMiddleware, order.getCart); 
 
+    // 33. 카트에 담긴 정보 조회API
+    app.get('/app/orders/:userId/in-cart', jwtMiddleware, order.getCart);
 };
