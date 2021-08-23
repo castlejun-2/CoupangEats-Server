ac2f5f9422d95ebe4c8fe06c27afb3fe80ac17a5
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-08-23 21:39:44 +0900

    Update

--- src/app/Order/orderController.js
+++ src/app/Order/orderController.js
@@ -184,7 +184,6 @@ const {emit} = require("nodemon");
         
         if(couponInfo[0].couponCount > 0){
             const userCouponInfo = await userProvider.getCoupon(userId, couponId, sumprice);
-            console.log(userCouponInfo[0])
             sumprice=sumprice-userCouponInfo[0].saleCost
         }
         result.push({'Total Cost': sumprice});
