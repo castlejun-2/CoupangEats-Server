92ab0f2d023e3536e40886bf0105c6ea8b6ea114
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-08-25 00:38:01 +0900

    Update storeService.js

--- src/app/Store/storeService.js
+++ src/app/Store/storeService.js
@@ -107,7 +107,7 @@ exports.updateUserReview = async function (userId, orderId, reviewId, starValue,
             connection.release();
             return response(baseResponse.SUCCESS);
         }
-        else if(!text){ //평점만 수정하는 경우
+        else if(!review){ //평점만 수정하는 경우
             const updateStarRatingReview = await storeDao.updateOnlyStarValueReviewInfo(connection, reviewId, starValue);
             connection.release();
             return response(baseResponse.SUCCESS);
