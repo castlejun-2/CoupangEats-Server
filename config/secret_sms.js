f1d84990b8b0d0c39fd0dd162b9586668d1a2d05
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-08-26 03:44:44 +0900

    UPDATE

--- config/secret_sms.js
+++ config/secret_sms.js
@@ -1,5 +1,3 @@
 module.exports = {
-    'NCP_serviceID': 'ncp:sms:kr:271185330164:coupangeats_project',
-    'NCP_accessKey': 'GnXhRBJuQlI2XljSG6zE',
-    'NCP_secretKey': '65iLbbbR16GH1O1aAg5Rf8ClK8S2LTNRSomlHrtP'
+
 };
