794fd9ee4550449761df22f925fe712369afc5b7
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-08-28 13:11:16 +0900

    index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('./config/express');
 const {logger} = require('./config/winston');
 
 let port;
-if(process.env.NODE_ENV == 'development'){
+
+if(process.env.NODE_ENV == 'development'){ //Developmnet Port Number
     port = 3000;
 }
-else if(process.env.NODE_ENV == 'production'){
+else if(process.env.NODE_ENV == 'production'){ //Production Port Number
     port = 3001;
 }
 express().listen(port);
